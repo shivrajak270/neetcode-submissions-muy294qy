3c2d1d43715168d229008726d28a96602d13b9bd
shivrajak270 <147083124+shivrajak270@users.noreply.github.com>
2026-04-13 21:27:04 +0530

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    int solve(int arr[],int i,int dp[]){
+         if (i >= arr.length)
+            return 0;
+
+        if(dp[i]!=-1){
+            return dp[i];
+        }    
+        int keep=arr[i]+solve(arr,i+2,dp);
+        int nokeep=solve(arr,i+1,dp);
+        return dp[i]= Math.max(keep,nokeep);
+
+    }
+    public int rob(int[] nums) {
+        int []dp=new int[nums.length];
+        Arrays.fill(dp, -1);
+        return solve(nums,0,dp);
+    }
+}
