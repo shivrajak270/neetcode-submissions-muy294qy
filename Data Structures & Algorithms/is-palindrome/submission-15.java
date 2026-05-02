ffa6b0d46cae7899e75cb4982d7d4e7da1d4ad1c
shivrajak270 <147083124+shivrajak270@users.noreply.github.com>
2026-05-02 23:23:00 +0530

    Add: is-palindrome - submission-15

---
+++ Data Structures & Algorithms/is-palindrome/submission-15.java
@@ -0,0 +1,18 @@
+class Solution {
+
+    public boolean isPalindrome(String s) {
+
+        StringBuilder sb= new StringBuilder();
+
+        for(int i=0;i<s.length();i++){
+            if(Character.isLetterOrDigit(s.charAt(i))){
+                sb.append(Character.toLowerCase(s.charAt(i)));
+            }
+        }
+        String h=sb.toString();
+        String j=sb.reverse().toString();
+        if(h.equals(j))
+          return true;
+        return false;
+    }
+}
