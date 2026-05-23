69d55c0cc45c1f71a8b0c93d09f6d5dfae8e1a68
shivrajak270 <147083124+shivrajak270@users.noreply.github.com>
2026-05-23 08:35:59 +0530

    Add: meeting-schedule - submission-5

---
+++ Data Structures & Algorithms/meeting-schedule/submission-5.java
@@ -0,0 +1,34 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+
+           intervals.sort((a, b) -> a.start - b.start);
+
+        for(int i=0;i<intervals.size()-1;i++){
+            int j=i+1;
+           // int start1=intervals.get(i).start;
+            int end1=intervals.get(i).end;
+            int start2=intervals.get(j).start;
+            //int end2=intervals.get(j).end;
+
+       
+            if(start2<end1)
+              return false;
+        }
+        return true;
+        
+
+
+
+    }
+}
