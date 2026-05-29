e71b4c3f2a1c42d94f23572a4fb85c41f1ce0644
shivrajak270 <147083124+shivrajak270@users.noreply.github.com>
2026-05-29 15:47:49 +0530

    Add: validate-parentheses - submission-16

---
+++ Data Structures & Algorithms/validate-parentheses/submission-16.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isValid(String s) {
+
+        int left=0;
+        int right=s.length()-1;
+        Stack<Character>hj=new Stack<>();
+        while(left<=right){
+            if(s.charAt(left)=='(' || s.charAt(left)=='{' || s.charAt(left)=='['){
+                hj.push(s.charAt(left));
+            }
+            else{
+                if(hj.isEmpty())
+                   return false;
+
+                char temp=hj.peek();
+                char leftchar=s.charAt(left);
+                if((temp=='(' && leftchar==')') || (temp=='[' && leftchar==']') || (temp=='{' && leftchar=='}'))
+                      hj.pop();
+                else{
+                    return false;
+                }    
+            }
+            left++;
+        }
+        if(!hj.isEmpty())
+           return false;
+        return true;
+        
+    }
+}
